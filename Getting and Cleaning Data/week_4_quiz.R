971457152f7ec5ba5ba2bb58930ec65a6be271e7
Yug <wakade.yugandhar@iitg.ac.in>
2018-11-08 16:50:05 +0530

    week 4 getting and cleaning data quiz

--- Getting and Cleaning Data/week_4_quiz.R
+++ Getting and Cleaning Data/week_4_quiz.R
@@ -44,6 +44,45 @@ q3 <- function()
                             nrows = 190,
                             select =c(1,2,4,5),
                             col.names = c("countryNames","rank","economy","GDP"))
+grep('^United',data$economy)
+}
+
+q4 <- function()
+{
+  if(!file.exists("./FGDP.csv"))
+  {
+    URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FGDP.csv"
+    download.file(URL, destfile = "./FGDP.csv")
+  }
   
-  grep("^a",data[,countryNames])
+  if(!file.exists("./F_Country.csv"))
+  {
+    URL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FEDSTATS_Country.csv"
+    download.file(URL, destfile = "./F_Country.csv")
+  }
+  
+  FGDP <- data.table::fread('./FGDP.csv'
+                            ,skip = 5
+                            ,nrows = 190
+                            ,select = c(1,2,4,5)
+                            ,col.names = c("CountryCode","Rank", "Country", "GDP"))
+  F_country <- data.table::fread('./F_Country.csv')
+  
+  merge_dt <- merge(FGDP,F_country,by = 'CountryCode')
+  
+  merge_dt[grepl(pattern = 'Fiscal year end: June 30;',merge_dt[,`Special Notes`]),.N]
+  
+}
+
+q5 <- function()
+{
+  library('quantmod')
+  amzn = getSymbols("AMZN",auto.assign=FALSE)
+  sampleTimes = index(amzn)
+  
+  tab <- data.table::data.table(timeCol = sampleTimes)
+  
+  tab[(timeCol >= "2012-01-01") & (timeCol) < "2013-01-01", .N ]
+ 
+  tab[((timeCol >= "2012-01-01") & (timeCol < "2013-01-01")) & (weekdays(timeCol) == "Monday"), .N ] 
 }
